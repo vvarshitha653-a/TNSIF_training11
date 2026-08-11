d2608de8535cfac5504f961933be2894fd2fb8a3
vvarshitha653-a <vvarshitha653@gmail.com>
2026-08-11 14:57:15 +0530

    first commit

---
+++ TNSIF_Training/src/org/tnsif/acc/c2tc/interfacedemo/SmartDevice.java
@@ -0,0 +1,12 @@
+package org.tnsif.acc.c2tc.interfacedemo;
+
+public interface SmartDevice {
+	
+	public abstract void turnOn();
+	public abstract  void turnOff();
+	public abstract  void getStatus();
+	
+	
+	
+
+}
