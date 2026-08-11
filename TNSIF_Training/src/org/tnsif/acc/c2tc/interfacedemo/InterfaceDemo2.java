3aee5abd2a3dc2a67bc6c8d7ab950d3dc0a6b5c9
vvarshitha653-a <vvarshitha653@gmail.com>
2026-08-11 15:00:59 +0530

    first commit

--- TNSIF_Training/src/org/tnsif/acc/c2tc/interfacedemo/InterfaceDemo2.java
+++ TNSIF_Training/src/org/tnsif/acc/c2tc/interfacedemo/InterfaceDemo2.java
@@ -1,5 +1,30 @@
 package org.tnsif.acc.c2tc.interfacedemo;
 
-public class InterfaceDemo2 {
+interface MathOp {
+    int operate(int a, int b);
+}
 
+class Add implements MathOp {
+    public int operate(int a, int b) {
+        return a + b;
+    }
+}
+
+class Sub implements MathOp {
+    public int operate(int a, int b) {
+        return a - b;
+    }
+}
+
+public class InterfaceDemo2 {
+    public static void main(String[] args) {
+
+        MathOp obj;
+
+        obj = new Add();
+        System.out.println(obj.operate(10, 5));  // 15
+
+        obj = new Sub();
+        System.out.println(obj.operate(10, 5));  // 5
+    }
 }
