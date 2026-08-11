9e46b6deafcc1c54e0fe851b202de784144a9d33
vvarshitha653-a <vvarshitha653@gmail.com>
2026-08-11 15:17:38 +0530

    first commit

---
+++ TNSIF_Training/src/org/tnsif/acc/c2tc/methodoverriding_super_this_instanceof/ThisDemo2.java
@@ -0,0 +1,30 @@
+package org.tnsif.acc.c2tc.methodoverriding_super_this_instanceof;
+
+class ProductBottle
+{
+	double price;
+	
+	ProductBottle(double price)
+	{
+		this.price=price;
+	}
+	double CalculateDiscount()
+	{
+		return this.price*0.10;
+	}
+	double calculateFinalPrice()
+	{
+		return this.price-this.CalculateDiscount();
+	}
+}
+public class ThisDemo2 {
+
+	public static void main(String[] args) {
+		ProductBottle product=new ProductBottle(400.00);
+		System.out.println("Final Price :"+ product.calculateFinalPrice());
+		
+		
+
+	}
+
+}
